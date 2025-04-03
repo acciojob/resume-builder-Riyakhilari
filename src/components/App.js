233afbbf62f490c83b17b59f7a9f510796d8e5e8
Riya <149504570+Riyakhilari@users.noreply.github.com>
2025-04-03 20:43:53 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,13 @@
+
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Profile from "./components/Profile";
-import Education from "./components/Education";
-import Skills from "./components/Skills";
-import Projects from "./components/Projects";
-import SocialMedia from "./components/SocialMedia";
-import FinalResume from "./components/FinalResume";
-import "./../styles/App.css";
+import './../styles/App.css';
 
 const App = () => {
   return (
-    <Router>
-      <div>
-        {/* Navigation between sections */}
-        <Routes>
-          <Route path="/" element={<Profile />} />
-          <Route path="/education" element={<Education />} />
-          <Route path="/skills" element={<Skills />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/social-media" element={<SocialMedia />} />
-          <Route path="/final-resume" element={<FinalResume />} />
-        </Routes>
-      </div>
-    </Router>
-  );
-};
+    <div>
+        {/* Do not remove the main div */}
+    </div>
+  )
+}
 
-export default App;
+export default App
